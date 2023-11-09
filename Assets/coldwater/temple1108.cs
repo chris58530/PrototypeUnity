cbeb7d61536aa1f615edb21c7edd1e5316847805
coldwaterzx <112992767+coldwaterzx@users.noreply.github.com>
2023-11-09 22:03:52 +0800

    1109

--- Assets/coldwater/temple1108.cs
+++ Assets/coldwater/temple1108.cs
@@ -12,9 +12,13 @@ public class temple1108 : MonoBehaviour
     public float tolerance = 0.05f; // 寬容區間
     // [SerializeField] private Image timerImg;//節奏中心圖
     // [SerializeField] private Slider timerBar;//節奏節點
-
+    [SerializeField] private Slider comboBar;
     public RhythmTimer rhythmTimer;
     public Animator hitDetec;
+
+    public float dashDistance = 10f;
+    public float dashDuration = 0.5f;
+    private bool isDashing = false;
 
 
     void Start()
@@ -54,8 +58,59 @@ public class temple1108 : MonoBehaviour
                 hitDetec.Play("Fail", -1, 0);
             }
         }
+        if (Input.GetKeyDown(KeyCode.Q) && !isDashing)
+        {
+            Dash();
+            float AniTime = rhythmTimer.AniTime();
+            bool onBeat = rhythmTimer.IsOnBeat();
+            if (onBeat)
+            {
+                comboBar.value += 1;
+                Debug.Log("Perfect!" + AniTime);
+                hitDetec.Play("Perfect", -1, 0);
+            }
+            else
+            {
+                comboBar.value = 0;
+                Debug.Log("Missed the beat!" + AniTime);
+                hitDetec.Play("Fail", -1, 0);
+            }
+        }
 
 
+    }
+    void Dash()
+    {
+
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        LayerMask mask = 1 << LayerMask.NameToLayer("Ground");
+        var targetPosition = Vector3.zero;
+        if (Physics.Raycast(ray, out RaycastHit hit, 1000, mask))
+        {
+            Debug.DrawLine(ray.origin, hit.point);
+            targetPosition = hit.point;
+            targetPosition.y = transform.position.y;
+            Debug.Log(hit.point);
+        }
+
+        Vector3 dashDirection = (targetPosition - transform.position).normalized;
+
+        StartCoroutine(PerformDash(dashDirection));
+    }
+    IEnumerator PerformDash(Vector3 dashDirection)
+    {
+        isDashing = true;
+        Vector3 endPosition = transform.position + dashDirection * dashDistance;
+
+        float elapsedTime = 0f;
+
+        while (elapsedTime < dashDuration)
+        {
+            transform.position = Vector3.Lerp(transform.position, endPosition, elapsedTime / dashDuration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        isDashing = false;
     }
 
 }
