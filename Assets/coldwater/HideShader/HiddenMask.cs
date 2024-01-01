247a04000bcca97a2591b960d5764780663b9bb9
coldwaterzx <112992767+coldwaterzx@users.noreply.github.com>
2024-01-02 01:54:14 +0800

    Fix water in boat
    in scene not work
    start game the render shader
    start work

--- Assets/coldwater/HideShader/HiddenMask.cs
+++ Assets/coldwater/HideShader/HiddenMask.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class BrdgePaillarHidden : MonoBehaviour
+//使用方法:將此腳本掛在遮罩上(你想要讓東西變透明的區域)以及套用HiddenMask(會讓此物件變透明)
+//然後將你想遮蔽的物件掛入到此腳本的list中
+public class HiddenMask : MonoBehaviour
 {
     public GameObject[] ObjMasked;
     void Start()
