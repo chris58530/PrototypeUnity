323d36caae4547fd79aa149d17bb5a690e961636
coldwaterzx <112992767+coldwaterzx@users.noreply.github.com>
2024-03-05 02:33:40 +0800

    Cloud

---
+++ Assets/!/Art/UI/Heart/HeartTest.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeartTest : MonoBehaviour
+{
+    public int health;
+    public int numOfHearts;
+
+    public GameObject[] hearts;
+    public 
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
