11567740d554e463f9389bed21c2074fc912dc47
coldwaterzx <112992767+coldwaterzx@users.noreply.github.com>
2024-03-05 05:52:45 +0800

    HeartUI

--- Assets/!/Art/UI/Heart/HeartTest.cs
+++ Assets/!/Art/UI/Heart/HeartTest.cs
@@ -1,23 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
+using BehaviorDesigner.Runtime.Tasks.Unity.UnityInput;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 using UnityEngine.UI;
 
 public class HeartTest : MonoBehaviour
 {
-    public int health;
+    public int health = 4;
     public int numOfHearts;
 
     public GameObject[] hearts;
-    public 
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public Animator HeartUI;
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            TakeDamage();
+            HeartUI.Play("HurtShaking");
+        }
+
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            Heal();
+        }
+    }
+
+    void TakeDamage()
+    {
+        health--;
+        health = Mathf.Clamp(health, 0, numOfHearts); 
+        UpdateHearts();
+    }
+
+    void Heal()
+    {
+        health++;
+        health = Mathf.Clamp(health, 0, numOfHearts); 
+        UpdateHearts();
+    }
+
+    void UpdateHearts()
+    {
+        for (int i = 0; i < numOfHearts; i++)
+        {
+            if (i < health)
+            {
+                hearts[i].SetActive(true);
+                Transform heartTransform = hearts[i].transform.Find("Heart");
+                Animator animator = heartTransform?.GetComponent<Animator>();
+                animator.Play("Heal");
+            }
+            else
+            {
+                Transform heartTransform = hearts[i].transform.Find("Heart");
+                Animator animator = heartTransform?.GetComponent<Animator>();
+
+                if (animator != null)
+                {
+                    animator.Play("HeartBreaking");
+                }          
+            }
+        }
     }
 }
