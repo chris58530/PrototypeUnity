09e31a0faa58b1b768915b333390781737d2b0f7
小仔 <chris58530@gmail.com>
2024-03-06 02:04:35 +0800

    rerer

---
+++ Assets/!/Scripts/Level/BossAEndEvelator.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using _.Scripts;
+using UnityEngine;
+
+public class BossAEndEvelator : MonoBehaviour
+{
+    [SerializeField] private GameObject evelator;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            StartCoroutine(MoveDownCoroutine(5));
+            other.transform.parent = transform;
+            GameManager.Instance.SwitchScene(2);
+        }
+    }
+
+    private IEnumerator MoveDownCoroutine(float duration)
+    {
+        float elapsedTime = 0f;
+
+        while (elapsedTime < duration)
+        {
+            evelator.transform.position += new Vector3(0, -8 * Time.deltaTime, 0);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+    }
+}
