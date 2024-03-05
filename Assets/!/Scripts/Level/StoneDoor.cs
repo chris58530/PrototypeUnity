6339264d7f8d6a21f052c024a17959d93be61ffa
coldwaterzx <112992767+coldwaterzx@users.noreply.github.com>
2024-03-06 02:16:31 +0800

    STONE

--- Assets/!/Scripts/Level/StoneDoor.cs
+++ Assets/!/Scripts/Level/StoneDoor.cs
@@ -6,8 +6,11 @@ using UnityEngine;
 public class StoneDoor : MonoBehaviour,IDamageable
 {
     [SerializeField] private GameObject destroyObj;
+    [SerializeField] private GameObject StoneBreakVFX;
     public void OnTakeDamage(int value)
     {
+        AudioManager.Instance.PlaySFX("HitOnBigRock");
+        Instantiate(StoneBreakVFX, transform.position, Quaternion.identity);
         OnDied();
     }
 
