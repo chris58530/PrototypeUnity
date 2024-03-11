c8853bec58c7807bbc0b1e7dd093a7e391935f88
小仔 <chris58530@gmail.com>
2024-03-11 19:33:56 +0800

    shake

--- Assets/!/Scripts/UI/ShieldUI.cs
+++ Assets/!/Scripts/UI/ShieldUI.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class ShieldUI : MonoBehaviour
 {
@@ -17,7 +18,40 @@ public class ShieldUI : MonoBehaviour
     public void HitShield()
     {
         //shake all shield ui
-        _thisAni.Play("ShieldShaking");
+        // _thisAni.Play("ShieldShaking");
+
+
+        StartCoroutine(ShakeAnimatorObjec());
+    }
+
+    IEnumerator ShakeAnimatorObjec()
+    {
+        Vector3[] pos = new Vector3[shieldAni.Length];
+        for (int i = 0; i < shieldAni.Length; i++)
+        {
+            pos[i] = shieldAni[i].transform.position;
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            float x = Random.Range(-.5f, .5f);
+            float y = Random.Range(- .5f, .5f);
+
+            for (int j = 0; j < shieldAni.Length; j++)
+            {
+                shieldAni[j].transform.position = new Vector3(shieldAni[j].transform.position.x + x,
+                    shieldAni[j].transform.position.y + y, shieldAni[j].transform.position.z);
+            }
+
+            yield return new WaitForSeconds(0.03f);
+        }
+
+        for (int i = 0; i < shieldAni.Length; i++)
+        {
+            shieldAni[i].transform.position = pos[i];
+        }
+
+        yield return null;
     }
 
     public void BreakShield(int shieldNumber)
