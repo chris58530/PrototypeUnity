6aaea32fd82fcffd60e50877ce32fe796f727089
小仔 <chris58530@gmail.com>
2024-03-28 21:40:02 +0800

    Cuttle keymonster fix

--- Assets/!/Scripts/Enemy/KeyMonsterBase.cs
+++ Assets/!/Scripts/Enemy/KeyMonsterBase.cs
@@ -1,5 +1,6 @@
 using System;
 using _.Scripts.Enemy;
+using BehaviorDesigner.Runtime;
 using UniRx;
 using UnityEngine;
 using UnityEngine.Events;
@@ -8,6 +9,13 @@ using UnityEngine.UI;
 
 public class KeyMonsterBase : Enemy, ITaskResult
 {
+    [SerializeField] private bool canAttack;
+
+    public void Start()
+    {
+        bt.SetVariable("CanAttack",(SharedBool)canAttack);
+    }
+
     public void OnTakeDamage()
     {
         bt.SendEvent("GetHurt");
