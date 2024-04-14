4c8ed1d5b27a3dd4fee6ba59065bd9ef1a4dfb39
小仔 <chris58530@gmail.com>
2024-04-15 00:24:37 +0800

    k

--- Assets/!/Scripts/Level/Level_2/Torch.cs
+++ Assets/!/Scripts/Level/Level_2/Torch.cs
@@ -3,40 +3,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UniRx;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Torch : MonoBehaviour
 {
     [SerializeField] private GameObject[] openFire;
     [SerializeField] private GameObject golbinPrefab;
 
-    [SerializeField]private bool _isOpenFire;
-    private IDisposable _spawnGoblinDisposable;
+    [SerializeField] private bool isSpawnGlobin;
 
     public void OpenTorchLight()
     {
         foreach (var fire in openFire)
         {
             fire.SetActive(true);
-
         }
-      
     }
 
     public void CloseTorchLight()
     {
-        _isOpenFire = false;
-        _spawnGoblinDisposable?.Dispose();
         foreach (var fire in openFire)
         {
             fire.SetActive(false);
+        }
 
-        }            Instantiate(golbinPrefab, transform.position + Vector3.up*5, transform.rotation);
+        if (!isSpawnGlobin)
+            Instantiate(golbinPrefab, transform.position + Vector3.up * 5, transform.rotation);
 
-        // _spawnGoblinDisposable = Observable.Interval(TimeSpan.FromSeconds(1f)).Subscribe(_ =>
-        // {
-        //     // Instantiate(golbinPrefab, transform.position + Vector3.up*5, transform.rotation);
-        // }).AddTo(this);
+        isSpawnGlobin = true;
     }
-
-  
 }
